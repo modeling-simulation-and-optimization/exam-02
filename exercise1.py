a795a1184e89ca227f7f4009cca3e49b23fb191c
Juan Romero <juan.romero1201@gmail.com>
2022-11-17 10:04:27 -0500

    Feat: Add exercise 1 and 4

---
+++ exercise1.py
@@ -0,0 +1,109 @@
+"""
+    Parcial 2 - MOS
+    Ejercicio 1
+
+    Realizado por:
+    Juan Andrés Romero C - 202013449
+    Juan Sebastián Alegría - 202011282
+"""
+
+import matplotlib.pyplot as plt
+
+from pyomo.environ import *
+from pyomo.opt import SolverFactory
+
+def delete_component(Model, comp_name):
+
+    list_del = [vr for vr in vars(Model)
+                if comp_name == vr
+                or vr.startswith(comp_name + '_index')
+                or vr.startswith(comp_name + '_domain')]
+
+    list_del_str = ', '.join(list_del)
+    print('Deleting model components ({}).'.format(list_del_str))
+
+    for kk in list_del:
+        Model.del_component(kk)
+
+Model = ConcreteModel()
+
+Model.Nodes = RangeSet(1, 5)
+
+Model.Hops = Param(Model.Nodes, Model.Nodes, mutable=True)
+Model.Costs = Param(Model.Nodes, Model.Nodes, mutable=True)
+
+for i in Model.Nodes:
+    for j in Model.Nodes:
+        Model.Hops[i,j] = 999
+        Model.Costs[i,j] = 999
+
+Model.Hops[1, 2] = 1
+Model.Hops[1, 3] = 1
+Model.Hops[2, 5] = 1
+Model.Hops[3, 4] = 1
+Model.Hops[4, 5] = 1
+
+Model.Costs[1, 2] = 10
+Model.Costs[1, 3] = 5
+Model.Costs[2, 5] = 10
+Model.Costs[3, 4] = 5
+Model.Costs[4, 5] = 5
+
+Model.X = Var(Model.Nodes, Model.Nodes, domain=Binary)
+
+Model.f1 = sum(Model.Hops[i,j] * Model.X[i,j] for i in Model.Nodes for j in Model.Nodes)
+Model.f2 = sum(Model.Costs[i,j] * Model.X[i,j] for i in Model.Nodes for j in Model.Nodes)
+
+Model.obj = Objective(expr= Model.f2, sense=minimize)
+
+# Restricción nodo origen
+def source_restriction(Model, i):
+    if i == 1:
+        return sum(Model.X[i, j] for j in Model.Nodes) == 1
+    else:
+        return Constraint.Skip
+
+Model.source = Constraint(Model.Nodes, rule=source_restriction)
+
+# Restricción nodo destino
+def destination_restriction(Model, j):
+    if j == 5:
+        return sum(Model.X[i, j] for i in Model.Nodes) == 1
+    else:
+        return Constraint.Skip
+
+Model.destination = Constraint(Model.Nodes, rule=destination_restriction)
+
+# Restricción nodo intermedio
+def intermediate_restriction(Model, i):
+    if i != 1 and i != 5:
+        return sum(Model.X[i, j] for j in Model.Nodes) - sum(Model.X[j, i] for j in Model.Nodes) == 0
+    else:
+        return Constraint.Skip
+
+Model.intermediate = Constraint(Model.Nodes, rule=intermediate_restriction)
+
+f1_const = 5
+f1_vec = []
+f2_vec = []
+
+while f1_const >= 2:
+
+    def f1_constraint(Model):
+        return Model.f1 <= f1_const
+
+    Model.f1_constraint = Constraint(rule=f1_constraint)
+
+    SolverFactory('glpk').solve(Model)
+    f1_vec.append(value(Model.f1))
+    f2_vec.append(value(Model.f2))
+    f1_const -= 1
+    delete_component(Model, 'f1_constraint')
+
+plt.plot(f1_vec, f2_vec, 'o-.')
+plt.title('Frente óptimo de Pareto')
+plt.xlabel('F1')
+plt.ylabel('F2')
+
+plt.grid(True)
+plt.show()
---
+++ exercise4.py
@@ -0,0 +1,88 @@
+"""
+    Parcial 2 - MOS
+    Ejercicio 4
+
+    Realizado por:
+    Juan Andrés Romero C - 202013449
+    Juan Sebastián Alegría - 202011282
+"""
+
+import random
+import math
+
+def second_max_min(list, mode):
+    if mode == 'max':
+        temp_list = [x for x in list if x != max(list)]
+        return max(temp_list)
+    elif mode == 'min':
+        temp_list = [x for x in list if x != min(list)]
+        return min(temp_list)
+
+class ZValues:
+
+    def __init__(self, point, z):
+        self.point = point
+        self.z = z
+
+    def __lt__(self, other):
+        return self.z < other.z
+
+    def __gr__(self, other):
+        return self.z > other.z
+
+    @classmethod
+    def find_z(cls, values, z):
+        counter = 0
+        for value in values:
+            if value.z == z.z:
+                return counter
+            counter += 1
+
+
+def simplex(fev_list:list[tuple], z):
+
+    selected_fev = random.choice(fev_list)
+
+    while True:
+        index_selected_fev = fev_list.index(selected_fev)
+        # Create graph
+        graph = [[999 for _ in range(len(fev_list))] for _ in range(len(fev_list))]
+        i, j = 0, 0
+        for x in fev_list:
+            for y in fev_list:
+                distance = math.sqrt(((x[0] - y[0])**2) + ((x[1] - y[1])**2))
+                if distance == 0:
+                    distance = 999
+                graph[i][j] = distance
+                j += 1
+            j = 0
+            i += 1
+
+        selected_distances = graph[index_selected_fev]
+        closest_fev_distance = min(selected_distances)
+        second_closest_distance = second_max_min(selected_distances, 'min')
+
+        closest_index = selected_distances.index(closest_fev_distance)
+        second_closest_index = selected_distances.index(second_closest_distance)
+        closest_fev = fev_list[closest_index]
+        second_closest_fev = fev_list[second_closest_index]
+
+        # Calculate the z value in the point
+        selected_z = z(selected_fev[0], selected_fev[1])
+        closest_z = z(closest_fev[0], closest_fev[1])
+        second_closest_z = z(second_closest_fev[0], second_closest_fev[1])
+
+        z_list = [ZValues(selected_fev, selected_z), ZValues(closest_fev, closest_z), ZValues(second_closest_fev, second_closest_z)]
+
+        best_z = max(z_list)
+
+        index = ZValues.find_z(z_list, best_z)
+        if index == 0:
+            return selected_fev, selected_z
+        selected_fev = best_z.point
+
+def objective_function(x, y):
+    return 3*x + 2*y
+
+result = simplex([(0,0), (40,0), (40,20), (20,60), (0, 80)], objective_function)
+print(result)
